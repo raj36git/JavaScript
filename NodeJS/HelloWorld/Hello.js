30366212c7e2fcfc818f6c2991a5d538250aec0d
Razz <vector36razz@gmail.com>
2024-01-07 15:59:48 +0545

    Node Module updated

--- NodeJS/HelloWorld/Hello.js
+++ NodeJS/HelloWorld/Hello.js
@@ -1,4 +1,4 @@
-console.log("Hey there!NodeJS");
+// console.log("Hey there!NodeJS");
 
 function add(a,b){
     return a + b;
@@ -6,6 +6,15 @@ function add(a,b){
 function sub(a,b){
     return a-b;
 }
+function mul(a,b){
+    return a*b;
+}
 // module.exports = add;
 // module.exports = sub;
-module.exports = {add,sub};
+// module.exports = mul; //this will result method overriding
+//so create
+module.exports = {
+    addFn: add,
+    subFn: sub,
+    mulFn: mul
+};
--- NodeJS/HelloWorld/module.js
+++ NodeJS/HelloWorld/module.js
@@ -1,7 +1,22 @@
-const {add,sub} = require('./Hello')
-console.log(add(2,3));
-console.log(sub(2,3));
+//built-in module
+const bulit = require('fs');
+console.log(bulit);
+
+// const {add,sub} = require('./Hello')
+// console.log(add(2,3));
+// console.log(sub(2,3));
 
 // const sub = require('./Hello')
 // console.log(sub(2,3));
+
+// const math = require('./Hello');
+// console.log(math.addFn(2,3));
+// console.log(math.subFn(2,3));
+// console.log(math.mulFn(2,3));
+
+//destructureing it
+const {addFn,subFn,mulFn} = require('./Hello')
+console.log(mulFn(3,3));
+
+
 
